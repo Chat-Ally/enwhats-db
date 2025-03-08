09ea0389dd7d7f369d3abf2e08ddc0c85101f16d
Daniel <daanielescobar@gmail.com>
2025-03-08 20:58:34 +0000

    log errors

--- src/phone-numbers.ts
+++ src/phone-numbers.ts
@@ -6,16 +6,25 @@ import type PhoneNumber from "./dto/phone-number.js";
  *
  * @param {string} customerPhone - A phone number from a customer.
 */
-export async function getOrCreatePhoneNumber(supabase: SupabaseClient, customerPhone: string): Promise<PhoneNumber | undefined> {
+export async function getOrCreatePhoneNumber(
+    supabase: SupabaseClient,
+    customerPhone: string
+): Promise<PhoneNumber | undefined> {
     try {
-        const { data, error } = await supabase.from("phones").select("*").eq("number", customerPhone).single();
-
-        if (error) throw new Error(error.message);
-
+        const { data, error } = await supabase
+            .from("phones")
+            .select("*")
+            .eq("number", customerPhone)
+            .single();
+        if (error) console.error(error.message);
         if (data) return data;
 
-        const { data: upsertData } = await supabase.from("phones").upsert([{ number: customerPhone }]);
+        const { data: upsertData, error: upsertError } = await supabase
+            .from("phones")
+            .upsert([{ number: customerPhone }]);
         if (upsertData) return upsertData[0];
+        if (upsertError) console.error(upsertError)
+
     } catch (error) {
         console.error('getOrCreatePhoneNumber', error);
         return undefined;
